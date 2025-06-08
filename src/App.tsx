24a7a4cd98b313bdb17a82f24ad23833888b51bf
Araf Hossain <arafh13@gmail.com>
2025-06-08 10:58:55 -0500

    Add basic landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import VerbForms from "./components/VerbForms";
 
 function App() {
+  const [section, setSection] = useState("home");
+
   return (
-    <div className="App">
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
       <h1>Arabic Helper</h1>
+      <p>Strengthen your Arabic practice with tailored exercises.</p>
+
+      {section === "home" && (
+        <div style={{ marginTop: "2rem" }}>
+          <button
+            onClick={() => {
+              setSection("verb-forms");
+            }}
+            style={{
+              padding: "1rem 2rem",
+              fontSize: "1rem",
+              cursor: "pointer",
+            }}
+          >
+            Verb Forms
+          </button>
+        </div>
+      )}
+
+      {section === "verb-forms" && <VerbForms />}
     </div>
   );
 }
