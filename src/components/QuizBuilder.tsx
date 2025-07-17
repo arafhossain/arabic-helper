07dfff66d5ee0698b8d8af70d09e1c00687e1de9
Araf Hossain <arafh13@gmail.com>
2025-07-17 08:47:33 -0500

    Add breadcrumb bar

--- src/components/QuizBuilder.tsx
+++ src/components/QuizBuilder.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import QuizBuilderForm from "./QuizBuilderForm";
 import CustomQuiz from "./CustomQuiz";
+import BreadcrumbBar from "./BreadcrumbBar";
 
 type Props = {};
 
@@ -11,21 +12,39 @@ export default function QuizBuilder({}: Props) {
 
   if (quizStarted) {
     return (
-      <CustomQuiz
-        selectedForms={selectedForms}
-        numQuestions={numQuestions}
-        onQuit={() => setQuizStarted(false)}
-      />
+      <div>
+        <BreadcrumbBar
+          trail={[
+            { label: "Home", path: "/" },
+            { label: "Verb Forms", path: "/verb-forms" },
+            { label: "Custom Quiz", path: null },
+          ]}
+        />
+        <CustomQuiz
+          selectedForms={selectedForms}
+          numQuestions={numQuestions}
+          onQuit={() => setQuizStarted(false)}
+        />
+      </div>
     );
   }
 
   return (
-    <QuizBuilderForm
-      selectedForms={selectedForms}
-      setSelectedForms={setSelectedForms}
-      numQuestions={numQuestions}
-      setNumQuestions={setNumQuestions}
-      onStart={() => setQuizStarted(true)}
-    />
+    <div>
+      <BreadcrumbBar
+        trail={[
+          { label: "Home", path: "/" },
+          { label: "Verb Forms", path: "/verb-forms" },
+          { label: "Custom Quiz", path: null },
+        ]}
+      />
+      <QuizBuilderForm
+        selectedForms={selectedForms}
+        setSelectedForms={setSelectedForms}
+        numQuestions={numQuestions}
+        setNumQuestions={setNumQuestions}
+        onStart={() => setQuizStarted(true)}
+      />
+    </div>
   );
 }
